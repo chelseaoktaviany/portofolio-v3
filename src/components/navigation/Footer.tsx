0ff8ebcc88e45c4f0a47966dd364783e9565af76
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2025-11-02 18:52:07 +0700

    moved footer to the navigation folder

---
+++ src/components/navigation/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer
+      className="row-start-3 flex gap-[24px] flex-wrap items-center
+        justify-center"
+    >
+      test
+    </footer>
+  );
+};
+
+export default Footer;
