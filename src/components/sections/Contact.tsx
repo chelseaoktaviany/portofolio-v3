09c22d20975787e6024383816ec12a3b0e3cf552
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2025-11-06 18:14:14 +0700

    update: updated contact

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
+// components
+import FormInput from "../FormInput";
+
 const Contact = () => {
   return (
-    <div>
-      <h1>contact</h1>
+    <div className="flex flex-col justify-center items-center mt-5">
+      <h1
+        className="text-6xl text-center md:text-center sm:text-center font-bold
+          font-(family-name:--font-archivo-black) py-10 mb-10"
+      >
+        Contact Us
+      </h1>
+      {/*form inputs*/}
+      <div className="w-full flex justify-center items-center mt-15">
+        <FormInput />
+      </div>
     </div>
   );
 };
