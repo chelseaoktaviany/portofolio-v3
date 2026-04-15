99639f03cd0ed437858ea3cc48cb7cc63b45041d
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2026-04-15 12:12:01 +0700

    fix: updated navbar and footer

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,4 +1,10 @@
-const Footer = () => {
+import { connection } from "next/server";
+
+const Footer = async () => {
+  await connection();
+
+  const year = new Date().getFullYear();
+
   return (
     <footer
       className="flex justify-center items-center h-20 lg:h-40 md:h-40 sm:h-20
@@ -8,7 +14,7 @@ const Footer = () => {
         className="text-sm lg:text-lg md:text-md sm:text-sm text-center
           font-light"
       >
-        &copy; 2022-{new Date().getFullYear()} chel.dev all rights reserved.
+        &copy; 2022-{year} chel.dev all rights reserved.
       </h1>
     </footer>
   );
